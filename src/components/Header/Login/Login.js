91479edc3a64ad9f2b1d340514911994e9a2f8a1
chessdude1 <mitia.platonow2011@yandex.ru>
2021-04-16 22:47:24 +0300

    first form in login page

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,5 +1,34 @@
+import {Field, reduxForm } from "redux-form"
+
+const LoginForm = (props) => {
+    return( <form onSubmit={props.handleSubmit}>
+         <div>
+             <Field placeholder={'Login'} name={'Login'} component={'input'}></Field>
+         </div>
+         <div>
+            <Field placeholder={'Password'} name={'Password'} component={'input'}></Field>
+         </div>
+         <div>
+            <Field component={'input'} name={'rememberMe'} type={'checkbox'}></Field>
+         </div>
+         <div>
+             <button>Login</button>
+         </div>
+     </form>)
+}
+
+
+const LoginReduxForm = reduxForm({
+    form: 'login'
+})(LoginForm)
+
 export const Login = () => {
-    return(
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return( <div>
         <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     )
 }
--- src/components/redux/redux-store.js
+++ src/components/redux/redux-store.js
@@ -4,14 +4,16 @@ import Experience_post_reducer from "./Experience_post_reducer";
 import {Contacts_messages_reducer} from "./Contacts_messages_reducer";
 import {Profile_reducer} from './Profile_Page_reducer'
 import { Auth_reducer } from "./auth_reducer";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
     Messages_page:   Messages_reducer,
     Experience_post:  Experience_post_reducer,
     Contacts_messages: Contacts_messages_reducer,
     Profile_page: Profile_reducer,
-    Auth : Auth_reducer
+    Auth : Auth_reducer,
+    form : formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
