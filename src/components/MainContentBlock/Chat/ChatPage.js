8411b4f58f7b6cda9d962e94efc5d8c2f546c2b4
chessdude1 <mitia.platonow2011@yandex.ru>
2021-05-22 15:04:16 +0300

    Right work off Websocket

--- src/components/MainContentBlock/Chat/ChatPage.js
+++ src/components/MainContentBlock/Chat/ChatPage.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-
-
-
 export const ChatPage = () => {
   return (
     <div>
@@ -12,63 +9,101 @@ export const ChatPage = () => {
 };
 
 const Chat = () => {
+    const [wsChannel, setWsChannel] = useState(null)
+
+    useEffect(()=> {
+      let ws
+
+      const closeHandler = ()=> {
+        console.log('Close')
+        setTimeout(CreateChannel, 3000)
+      }
+
+      function CreateChannel() {
+        ws?.removeEventListener('close', closeHandler)
+        ws?.close()
+        ws= new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+        if (ws !== null) {
+        }
+        setWsChannel(ws)
+        ws?.addEventListener('close', closeHandler) 
+      }
+
+      CreateChannel();
+
+      return ()=> {
+        ws?.removeEventListener('close', closeHandler) //предотвращение утечки памяти, отписка от всех событий //
+        ws.close()
+      }
+    }, [])
+    
   return (
     <div>
       <span>Its my chat</span>
-      <ChatMessages />
-      <AddMessageForm />
+      <ChatMessages ws={wsChannel} />
+      <AddMessageForm ws={wsChannel} />
     </div>
   );
 };
 
+const ChatMessages = ({ ws }) => {
+  const [TestArray, setMessages] = useState([]);
+  useEffect(() => {
+    let messageHandler = (e) => {
+      setMessages((prevMessages) => [...prevMessages, ...JSON.parse(e.data)]);
+    }
+    ws?.addEventListener("message", messageHandler);
 
+    return () => {
+      ws?.removeEventListener('message', messageHandler)
+    }
 
-const ChatMessages = () => {
-    const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-    const[TestArray, setMessages] = useState([])
-    useEffect(()=> {
-        ws.addEventListener('message', (e)=> {
-            setMessages((prevMessages) =>[...prevMessages, ...JSON.parse(e.data)])
-    
-        })
-    }, [])
- 
+  }, [ws]);
+
   return (
-        <div style={{height : '400px', overflowY : 'auto'}}>
-            {TestArray.map((u, index) => <Message key={index} message={u}/>)}
-        </div>);
+    <div style={{ height: "400px", overflowY: "auto" }}>
+      {TestArray.map((u, index) => (
+        <Message key={index} message={u} />
+      ))}
+    </div>
+  );
 };
 
 const Message = (props) => {
-    return (
-        <div>
-            <img src={props.message.photo}/>
-            <div>
-                {props.message.UserName}
-            </div>
-            <div>
-                {props.message.message}    
-            </div>
-        </div>
-    )
-}
-
-const AddMessageForm = () => {
-    const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-    const [message, setMessage] = useState('')
-
-    const sendMessage = ()=> {
-        if (!message) {
-            return
-        }
-        ws.send(message);
-        setMessage('')
-    }
   return (
     <div>
-      <textarea onChange={(e)=> setMessage(e.currentTarget.value)} value={message}></textarea>
+      <img src={props.message.photo} />
+      <div>{props.message.UserName}</div>
+      <div>{props.message.message}</div>
+    </div>
+  );
+};
+
+const AddMessageForm = ({ ws }) => {
+  const [message, setMessage] = useState("");
+  const [readyStatus, setReadyStatus] = useState("pending");
+
+  useEffect(() => {
+    ws?.addEventListener("open", () => {
+      setReadyStatus("ready");
+    });
+  }, [ws]);
+
+  const sendMessage = () => {
+    if (!message) {
+      return;
+    }
+    ws.send(message);
+    setMessage("");
+  };
+  return (
+    <div>
+      <textarea
+        onChange={(e) => setMessage(e.currentTarget.value)}
+        value={message}
+      ></textarea>
       <div>
-      <button onClick={sendMessage}>Send</button>
+        <button disabled ={readyStatus != 'ready'} onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
